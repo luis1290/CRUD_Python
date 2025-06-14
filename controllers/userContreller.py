206c1989f5396370dbd9a35302bd56d842fa5dd3
Luis <lgomez1290@gmail.com>
2025-06-13 19:10:56 -0600

    CRUD completado

--- controllers/userContreller.py
+++ controllers/userContreller.py
@@ -23,3 +23,37 @@ def getAllUsers():
   except Exception as e:
     return jsonify({"error": str(e)}), 400
 
+# ruta para obtener un usuario por ID
+@userBp.route('/getUserid/<int:userID>', methods=['GET'])
+def getUserById(userID):
+  try:
+      user = User.getUserById(userID) # obtiene un usuario por ID
+      if user:
+        return jsonify(user), 200 # devuelve el usuario encontrado
+      return jsonify({"error": "User not found"}), 404 # maneja el caso en que no se encuentra el usuario
+  except Exception as e:
+      return jsonify({"error": str(e)}), 400
+
+# ruta para eliminar usuario un usuario por ID
+@userBp.route('/deleteUser/<int:userID>', methods=['DELETE'])
+def deleteUser(userID):
+  try:
+    user = User.getUserById(userID) # busca el usuario por ID
+    if user:
+      User.deleteUserById(userID) # llama al método deleteUserById de la clase User
+      return jsonify({"message": "User deleted successfully"}), 200
+    return jsonify({"error": "User not found"}), 404 # maneja el caso en que no se encuentra el usuario
+  except Exception as e:
+    return jsonify({"error": str(e)}), 400
+  
+# ruta para actualizar un usuario por ID
+@userBp.route('/updateUser/<int:userID>', methods=['PUT'])
+def updateUser(userID):
+  try:
+    data = request.get_json() # obtiene los datos del usuario desde el cuerpo de la solicitud
+    update = User.updateUser(userID, data)
+    if update:
+      return jsonify({"message": "User updated successfully"}), 200
+    return jsonify({"error": "User not found"}), 404 # maneja el caso en que no se encuentra el usuario
+  except Exception as e:
+    return jsonify({"error": str(e)}), 400
--- models/users.py
+++ models/users.py
@@ -18,5 +18,37 @@ class User: # defininendo una clase User
       return list(cls.user.values()) # devuelve una lista de todos los usuarios
     except Exception as e:
       print(f"Error getting all users: {e}")
+
+  # obtener un usuario por su ID
+  @classmethod
+  def getUserById(cls, userID):
+    try:
+      return cls.user.get(userID) # devuelve el usuario con el ID especificado o None si no existe
+    except Exception as e:
+      return print(f"Error getting user by ID: {e}")
+  
+  # eliminar un usuario por su ID
+  @classmethod
+  def deleteUserById(cls, userID):
+    try:
+      if userID in cls.user:
+        cls.user.pop(userID)# elimina el usuario con el ID especificado
+    except Exception as e:
+      print(f"Error deleting user by ID: {e}")
+  
+  # actualizar un usuario por su ID
+  @classmethod
+  def updateUser(cls, userID, userData):
+    try:
+      if userID in cls.user:
+        cls.user[userID].update(userData)
+        return cls.user[userID]
+      return print("User not found")
+    except Exception as e:
+      print(f"Error updating user: {e}")
+
+  
+    
+
   
   
